3e7f6e4a06318c45b044fb389a0de7d2d89e114a
MLIPH\benigjo <Jonathan_Beniga@manulife.com>
2021-11-24 18:03:00 +0800

    mod

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,5 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 //@SpringBootApplication
 //@RestController
 public class DemoApplication {
@@ -11,9 +8,9 @@ public class DemoApplication {
 	//	SpringApplication.run(DemoApplication.class, args);
 	//}
 	
-	@GetMapping("/hello")
+	/*@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
-	}
+	}*/
 
 }
