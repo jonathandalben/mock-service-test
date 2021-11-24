03fd138d6ad9208d8c005d96332d5983672e473b
MLIPH\benigjo <Jonathan_Beniga@manulife.com>
2021-11-24 12:33:31 +0800

    mock-service-test base

---
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,56 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@SpringBootApplication
+@RestController
+public class GreetingController {
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingController.class, args);
+		//System.setProperty("javax.net.ssl.trustStore", "C:/Users/benigjo/Downloads/baeldung_public_cert.cer");
+	    //System.setProperty("javax.net.ssl.trustStorePassword", "secret");
+	}
+	
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+	@GetMapping("/index")
+	public String showUserList(Model model) {
+	    return "index";
+	}
+	
+	@PostMapping("/v2/common/notification/v1/markets/hkg/messages")
+	public String postNotificationV1() {
+		return "{\"msgid\":\"acda4616-16bd-43fb-b147-319e31479c8f\",\"status\":\"PUBLISHED\",\"errorcode\":null,\"description\":\"E-mail has been published.\"}";
+	}
+	
+	@PostMapping("/v2/common/notification/v2/PHL/WMT/email")
+	public String postNotificationV2() {
+		return "{\"msgid\":\"acda4616-16bd-43fb-b147-319e31479c8f\",\"status\":\"PUBLISHED\",\"errorcode\":null,\"description\":\"E-mail has been published.\"}";
+	}
+	
+	@GetMapping("/v2/common/notification/v1/markets/hkg/messages")
+	public String getNotificationV1() {
+		return "{\"msgid\":\"acda4616-16bd-43fb-b147-319e31479c8f\",\"status\":\"PUBLISHED\",\"errorcode\":null,\"description\":\"E-mail has been published.\"}";
+	}
+	
+	@GetMapping("/v2/common/notification/v2/PHL/WMT/email")
+	public String getNotificationV2() {
+		return "{\"msgid\":\"acda4616-16bd-43fb-b147-319e31479c8f\",\"status\":\"PUBLISHED\",\"errorcode\":null,\"description\":\"E-mail has been published.\"}";
+	}
+}
